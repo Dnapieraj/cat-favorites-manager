8b0c566be2a55b80edbe04bf124c7d82ae9db4b0
Daniel <d.napieraj06@gmail.com>
2025-10-14 19:08:38 +0200

    Add API integration for fetching favorite cats

---
+++ credentials.py
@@ -0,0 +1,3 @@
+headers = {
+    'x-api-key': 'live_wWTgJ7QjR3Atlk8te2UQBXhveDXjP1X5ybaqDTXUjFPMeEZaaJqx2DJBuzU5VaGW'
+}
---
+++ lAPI.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import webbrowser
+from pprint import pprint
+import credentials
+
+r = requests.get('https://api.thecatapi.com/v1/favourites/', headers=credentials.headers)
+
+try:
+    content = r.json()
+except json.decoder.JSONDecodeError:
+    print('Niepoprawny format', r.text)
+else:
+    print(content)
+    
+print('Zaloguj się - podaj login i hasło')
+userId = 'agh2m'
+name = 'Arkadiusz'
+
+print('Witaj', name)
+print("Twoje ulubione koty to:", favouriteCats)
